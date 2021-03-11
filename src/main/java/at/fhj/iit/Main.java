01b66dd1362c73c0c76cfd24e8a0a56addb2fd12
Andreas Steirer <andreas.steirer@edu.campus02.at>
2021-03-11 16:14:52 +0100

    update Main class

--- src/main/java/at/fhj/iit/Main.java
+++ src/main/java/at/fhj/iit/Main.java
@@ -2,13 +2,20 @@ package at.fhj.iit;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Liquid l = new Liquid("Wein", 0.125, 13);
         System.out.println(l.getName());
         System.out.println(l.getVolume());
 
-        Drink d = new SimpleDrink("Rotwein",l);
+        Drink d = new SimpleDrink("Rotwein", l);
         System.out.println(d);
+
+        System.out.println("-".repeat(80));
+        System.out.println("END OF ALREADY SPECIFIED CODE");
+        System.out.println("-".repeat(80));
+
+        SoftDrink softDrink = new SoftDrink("Orange juice", 0.5, Brand.COCA_COLA);
+        System.out.println(softDrink);
     }
 }
