a95f413f3b784bd7add3375b95cc0a6842bfef4f
Andreas Steirer <andreas.steirer@edu.campus02.at>
2021-04-15 19:14:59 +0200

    add SoftDrinkTests class

---
+++ src/test/java/at/fhj/iit/SoftDrinkTests.java
@@ -0,0 +1,95 @@
+package at.fhj.iit;
+
+import at.fhj.iit.custom.Brand;
+import at.fhj.iit.custom.SoftDrink;
+import at.fhj.iit.exception.ValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Contains test cases regarding the
+ * <code>SoftDrink</code> class and its implementations.
+ *
+ * @author Andreas Steirer
+ * @version 2.0
+ * @see SoftDrink
+ * @since 1.0
+ */
+@DisplayName("Testing Cocktail class")
+public class SoftDrinkTests {
+
+    private SoftDrink softDrink;
+
+    /*
+     * Phase: Setup
+     */
+    @BeforeEach
+    private void setup() {
+        softDrink = new SoftDrink("TestDrink", 12.45, Brand.NESQUIK);
+    }
+
+    // More or less important to test.
+    // Normally this is not tested separately.
+    @Test
+    @DisplayName("Testing toString implementation")
+    public void changeName() {
+        // Phases: Exercise
+        String newName = "NewTestDrink";
+        softDrink.setName(newName);
+        
+        // Phases: Verify
+        assertEquals(newName, softDrink.getName());
+    }
+
+    // More or less important to test.
+    // Normally this is not tested separately.
+    @Test
+    @DisplayName("Testing toString implementation")
+    public void changeVolume() {
+        // Phases: Exercise
+        double newVolume = 10.0;
+        softDrink.setVolume(newVolume);
+
+        // Phases: Verify
+        assertEquals(newVolume, softDrink.getVolume());
+    }
+
+    // More or less important to test.
+    // Normally this is not tested separately.
+    @Test
+    @DisplayName("Testing toString implementation")
+    public void changeBrand() {
+        // Phases: Exercise
+        Brand newBrand = Brand.LA_CROIX;
+        softDrink.setBrand(newBrand);
+
+        // Phases: Verify
+        assertEquals(newBrand, softDrink.getBrand());
+    }
+
+    @Test
+    @DisplayName("Testing fails at SoftDrink creation")
+    public void createInvalidSoftDrink() {
+        // Phases: Verify(Exception specification, Exercise)
+        assertThrows(
+                ValidationException.class,
+                () -> new SoftDrink("CheckerDrink", 0.0, Brand.COCA_COLA)
+        );
+    }
+
+    // More or less important to test.
+    // Normally this is very rarely tested separately.
+    @Test
+    @DisplayName("Testing toString implementation")
+    public void callToString() {
+        // Phases: Verify(Exercise)
+        assertEquals(
+                "The SoftDrink 'TestDrink' is manufactured by 'Nesquik' and got a standard volume of 12.45 liters.",
+                softDrink.toString()
+        );
+    }
+}
