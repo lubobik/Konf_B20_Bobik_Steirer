8a9073b8e7b6fdd568d28e26691989c61ccabb84
Andreas Steirer <andreas.steirer@edu.campus02.at>
2021-04-15 19:13:42 +0200

    update Brand with documentation and change wording

--- src/main/java/at/fhj/iit/custom/Brand.java
+++ src/main/java/at/fhj/iit/custom/Brand.java
@@ -1,5 +1,13 @@
 package at.fhj.iit.custom;
 
+/**
+ * Specifies a <code>Brand</code> enumeration with an
+ * also fixed name specification.
+ *
+ * @author Andreas Steirer
+ * @version 2.0
+ * @since 1.0
+ */
 public enum Brand {
     COCA_COLA("Coca Cola"),
     LA_CROIX("La Croix"),
@@ -8,14 +16,20 @@ public enum Brand {
     OTHER("Other"),
     ;
 
-    private final String title;
+    /*
+     * The specified name
+     */
+    private final String name;
 
-    Brand(String title) {
-        this.title = title;
+    Brand(String name) {
+        this.name = name;
     }
 
+    /**
+     * Output of a stringified <code>Brand</code> enum value
+     */
     @Override
     public String toString() {
-        return title;
+        return name;
     }
 }
