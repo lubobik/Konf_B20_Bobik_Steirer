d3f86fac9e51ff39d9b3cf483f52bcfd1cb329fd
Andreas Steirer <andreas.steirer@edu.campus02.at>
2021-06-05 20:08:54 +0200

    update documentation of CashRegisterManagement

--- src/main/java/at/fhj/iit/custom/cashRegister/CashRegisterManagement.java
+++ src/main/java/at/fhj/iit/custom/cashRegister/CashRegisterManagement.java
@@ -8,6 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Represents a CashRegister in form of a certain <code>DrinkSale</code> history.
+ * Next to managing the sales history (in this case only additions are allowed),
+ * there is also the possibility to retrieve and
+ * print reports from its corresponding sales history.
+ *
+ * @author Andreas Steirer
+ * @author Lukas Bobik
+ * @version 3.0
+ */
 public class CashRegisterManagement {
 
     /**
@@ -41,6 +51,11 @@ public class CashRegisterManagement {
         return salesHistory;
     }
 
+    /*
+     * Section:
+     * Management
+     */
+
     /**
      * Adds the passed <code>DrinkSale</code> to
      * the current <code>DrinkSale</code> history list.
@@ -61,6 +76,17 @@ public class CashRegisterManagement {
         salesHistory.addAll(drinkSales);
     }
 
+    /*
+     * Section:
+     * Report metric calculations
+     */
+
+    /**
+     * Retrieves the total of all non-alcoholic <code>Drink</code>s
+     * from the current <code>DrinkSale</code> history.
+     *
+     * @return the total of all non-alcoholic <code>Drink</code> sales
+     */
     public double retrieveTotalOfNonAlcoholicBeverages() {
         return DrinkUtils.roundToTwoDecimals(salesHistory
                 .stream()
@@ -106,6 +132,17 @@ public class CashRegisterManagement {
         );
     }
 
+    /**
+     * Retrieves the total of all <code>Drink</code>s by a potential date
+     * from the current <code>DrinkSale</code> history.
+     * <p>
+     * Fallback:
+     * In case of <code>soldAt</code> being <code>null</code>,
+     * it will return the total of all days.
+     *
+     * @param soldAt a potential date / day on which the sale occurred or <code>null</code>
+     * @return the total of all <code>Drink</code> sales on the passed date or of all days
+     */
     public double retrieveTotalByDate(LocalDate soldAt) {
         return soldAt != null ? DrinkUtils.roundToTwoDecimals(salesHistory
                 .stream()
@@ -115,6 +152,20 @@ public class CashRegisterManagement {
         ) : retrieveTotal();
     }
 
+    /**
+     * Retrieves the total of all <code>Drink</code>s
+     * sold by a potential <code>Operator</code>
+     * from the current <code>DrinkSale</code> history.
+     * <p>
+     * Fallback:
+     * In case of <code>soldBy</code> being <code>null</code>,
+     * it will return the total of all days.
+     *
+     * @param soldBy a potential <code>Operator</code> who achieved
+     *               the sale or <code>null</code>
+     * @return the total of all <code>Drink</code> sales for the potential
+     * <code>Operator</code> or of all <code>Operator</code>s
+     */
     public double retrieveTotalByOperator(Operator soldBy) {
         return soldBy != null ? DrinkUtils.roundToTwoDecimals(salesHistory
                 .stream()
@@ -124,6 +175,23 @@ public class CashRegisterManagement {
         ) : retrieveTotal();
     }
 
+    /**
+     * Retrieves the total of all <code>Drink</code>s
+     * sold at a potential date and by a potential <code>Operator</code>
+     * from the current <code>DrinkSale</code> history.
+     * <p>
+     * Fallback cases:
+     * - If both <code>soldAt</code> and <code>soldBy</code> are <code>null</code>, return the complete total
+     * - If <code>soldAt</code> is <code>null</code>, return the complete total for all <code>Operator</code>s
+     * - If <code>soldBy</code> is <code>null</code>, return the complete total for all dates / days
+     * - If both <code>soldAt</code> and <code>soldBy</code> are NOT <code>null</code>, return the specific total
+     *
+     * @param soldAt a potential date / day on which the sale occurred
+     *               (in case of <code>null</code>, all <code>Operator</code>s will be considered)
+     * @param soldBy a potential <code>Operator</code> who achieved the sale
+     *               (in case of <code>null</code>, all days / dates will be considered)
+     * @return the total of all <code>Drink</code> sales for the passed date and <code>Operator</code>
+     */
     public double retrieveTotalByDateAndOperator(LocalDate soldAt, Operator soldBy) {
         boolean isNoSaleDate = soldAt == null;
         boolean isNoOperator = soldBy == null;
@@ -145,6 +213,12 @@ public class CashRegisterManagement {
         );
     }
 
+    /**
+     * Retrieves the total of all <code>Drink</code>s
+     * sold and tracked within the current <code>DrinkSale</code> history.
+     *
+     * @return the total of all <code>Drink</code> sales
+     */
     public double retrieveTotal() {
         return DrinkUtils.roundToTwoDecimals(salesHistory
                 .stream()
@@ -153,6 +227,15 @@ public class CashRegisterManagement {
         );
     }
 
+    /*
+     * Section:
+     * Report printing / Formatted output
+     */
+
+    /**
+     * Prints the total of sales of all <code>Drink</code>s
+     * from the current <code>DrinkSale</code> history.
+     */
     public void printTotal() {
         DrinkUtils.printFormattedMetric(
                 "Total of all operators and days",
@@ -160,24 +243,44 @@ public class CashRegisterManagement {
         );
     }
 
+    /**
+     * Prints the total of all non-alcoholic <code>Drink</code>s
+     * from the current <code>DrinkSale</code> history.
+     */
     public void printNonAlcoholic() {
         DrinkUtils.printFormattedMetric("Total of non-alcoholic beverages",
                 retrieveTotalOfNonAlcoholicBeverages()
         );
     }
 
+    /**
+     * Prints the total of all weak alcoholic <code>Drink</code>s
+     * from the current <code>DrinkSale</code> history.
+     */
     public void printAlcoholicWeak() {
         DrinkUtils.printFormattedMetric("Total of intense alcoholic beverages",
                 retrieveTotalOfAlcoholicBeveragesWeak()
         );
     }
 
+    /**
+     * Prints the total of all intense alcoholic <code>Drink</code>s
+     * from the current <code>DrinkSale</code> history.
+     */
     public void printAlcoholicIntense() {
         DrinkUtils.printFormattedMetric("Total of weak alcoholic beverages",
                 retrieveTotalOfAlcoholicBeveragesIntense()
         );
     }
 
+    /**
+     * Prints the total of sales of all <code>Drink</code>s by
+     * a potential <code>Operator</code>
+     * from the current <code>DrinkSale</code> history.
+     *
+     * @param soldBy a potential <code>Operator</code> who achieved the sales.
+     *               In case of <code>null</code>, the total of overall sales is printed.
+     */
     public void printTotalByOperator(Operator soldBy) {
         if (soldBy != null) {
             DrinkUtils.printFormattedMetric(
@@ -187,6 +290,14 @@ public class CashRegisterManagement {
         } else printTotal();
     }
 
+    /**
+     * Prints the total of all <code>Drink</code>s
+     * of all <code>Operator</code>s separately for an optional
+     * specific day / date from the current <code>DrinkSale</code> history.
+     *
+     * @param soldAt a potential date / day on which the sale occurred.
+     *               In case of <code>null</code>, total of all days are printed.
+     */
     public void printTotalByEachOperator(LocalDate soldAt) {
         salesHistory
                 .stream()
@@ -195,6 +306,13 @@ public class CashRegisterManagement {
                 .forEach(operator -> printTotalByDateAndOperator(soldAt, operator));
     }
 
+    /**
+     * Prints the total of sales of all <code>Drink</code>s
+     * from the current <code>DrinkSale</code> history.
+     *
+     * @param soldAt a potential date / day on which the sale occurred.
+     *               In case of <code>null</code>, total of overall sales is printed.
+     */
     public void printTotalByDate(LocalDate soldAt) {
         if (soldAt != null) {
             DrinkUtils.printFormattedMetric(
@@ -204,6 +322,21 @@ public class CashRegisterManagement {
         } else printTotal();
     }
 
+    /**
+     * Prints the total of all <code>Drink</code>s
+     * sold by a potential <code>Operator</code> and on a potential day / date
+     * from the current <code>DrinkSale</code> history.
+     * <p>
+     * Cases:
+     * - If both <code>soldAt</code> and <code>soldBy</code> are <code>null</code>, the complete total is printed
+     * - If only <code>soldAt</code> is <code>null</code>, the complete total for all <code>Operator</code>s is printed
+     * - If only <code>soldBy</code> is <code>null</code>, the complete total for all dates / days is printed
+     * - If both <code>soldAt</code> and <code>soldBy</code> are NOT <code>null</code>, the specific total is printed
+     *
+     * @param soldAt a potential date / day on which the sale occurred.
+     *               In case of <code>null</code>, overall sales by a <code>Operator</code> gets printed.
+     * @param soldBy a potential <code>Operator</code> who achieved the sales
+     */
     public void printTotalByDateAndOperator(LocalDate soldAt, Operator soldBy) {
         String message;
         boolean isSaleDate = soldAt != null;
